e1214ccbb237d404c8786ad30f00493c8a371d82
YttriLab <46530638+YttriLab@users.noreply.github.com>
2020-01-08 10:05:45 -0500

    Add files via upload
    Sketch used to clean solenoids and fluid lines

---
+++ Arduino Code/Flush.ino
@@ -0,0 +1,33 @@
+/*
+This sketch can be used to drain reservoir and flush/clean lines and solenoid
+
+Opens solenoid for 100 seconds and closes it for 1 second continuously
+
+To clean lines/solenoid run 10% bleach through then flush with clean water a few times
+*/
+int solenoid = 13;
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to  for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);//0x27  0x3F
+
+void setup() {
+  pinMode(solenoid, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);//set corresponding baud rate in Ardunio Serial Monitor
+  lcd.backlight();
+  lcd.begin();
+}
+
+void loop() {
+  digitalWrite(solenoid, HIGH); lcd.clear(); lcd.print("open");
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("open");
+  delay(100000);
+  digitalWrite(solenoid, LOW); lcd.clear(); Serial.println("closed");
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("closed");
+  delay(1000);
+}
